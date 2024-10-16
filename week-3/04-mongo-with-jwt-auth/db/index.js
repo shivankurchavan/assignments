a007e5d6f17b84230588f6d11457d3ff744f9fac
shivankurchavan <shivankurchavan@gmail.com>
2024-10-16 23:55:18 +0530

    week3/04-complete

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,40 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://shiv:87654321@cluster0.q2tvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to the database');
+  });
+  
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to the database', err);
+});
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        default: []
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,75 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../config');
+const {Admin, Course} = require('../db');
+ 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const admin = new Admin({
+        username: username,
+        password: password,
+    })
+
+    admin.save();
+    res.json({
+        message: "Admin created successfully"
+    })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = Admin.findOne({
+        username: username,
+        password: password,
+    })
+    if (user){
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+        res.json({
+            message: "Signin successful",
+            token
+        })
+    }
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    
+    const newCourse = await Course.create({
+        title: title,
+        description: description,
+        imageLink: imageLink,
+        price: price
+    })
+    console.log(newCourse);
+    res.json({
+        message: "course created successfully", courseId: newCourse._id
+    })
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const response = await Course.find({});
+
+    res.json({
+        courses: response
+    });
 });
 
 module.exports = router;
